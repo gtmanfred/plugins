6bc6f7aa8b6a77056e7c0ef64bc8060f9cec88f2
Daniel Wallace <danielwallace@gtmanfred.com>
2018-04-20 15:00:32 -0500

    package html files

--- triagesched/setup.py
+++ triagesched/setup.py
@@ -7,16 +7,22 @@ packages = ['triagesched']
 
 with os.scandir('triagesched') as rit:
     for entry in rit:
-        if entry.name[0] not in ('.', '_') and entry.is_dir():
+        if entry.name[0] not in ('.', '_') and entry.is_dir() and os.path.isfile(f'{entry.path}/__init__.py'):
             packages.append(f'{entry.path.replace("/", ".")}')
 
 print(packages)
 
-setup(name='triagesched',
-      version='0.1',
-      description='Triage schedule',
-      author='Daniel',
-      author_email='daniel@gtmanfred.com',
-      url='https://github.com/gtmanfred/plugins',
-      packages=packages,
-     )
+setup(
+    name='triagesched',
+    version='0.1',
+    description='Triage schedule',
+    author='Daniel',
+    author_email='daniel@gtmanfred.com',
+    url='https://github.com/gtmanfred/plugins',
+    packages=packages,
+    include_package_data=True,
+    data_files=[
+        ('share/nginx/html/', ['triagesched/html/index.html']),
+        ('share/nginx/html/edit/', ['triagesched/html/edit/index.html']),
+    ],
+)
