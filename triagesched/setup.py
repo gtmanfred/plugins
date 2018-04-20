1e3952298d4ad30a1539e2e970b08bf74d7fe662
Daniel Wallace <danielwallace@gtmanfred.com>
2018-04-20 15:39:29 -0500

    rewrite url on install

--- triagesched/setup.py
+++ triagesched/setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import distutils.dist
+from distutils.command.install_data import install_data
+
 import os
 
 packages = ['triagesched']
@@ -10,11 +13,26 @@ with os.scandir('triagesched') as rit:
         if entry.name[0] not in ('.', '_') and entry.is_dir() and os.path.isfile(f'{entry.path}/__init__.py'):
             packages.append(f'{entry.path.replace("/", ".")}')
 
-print(packages)
+class InstallData(install_data):
+    def run(self):
+        install_data.run(self)
+        for pkgfile in self.outfiles:
+            with open(pkgfile, 'r') as tmpfile:
+                filedata = tmpfile.read()
+            filedata = filedata.replace('localhost:5000', 'triage.gtmanfred.com')
+            with open(pkgfile, 'w') as tmpfile:
+                print(filedata, file=tmpfile)
+
+
+class TriageDist(distutils.dist.Distribution):
+    def __init__(self, attrs=None):
+        distutils.dist.Distribution.__init__(self, attrs)
+        self.cmdclass.update({'install_data': InstallData})
 
 setup(
+    distclass=TriageDist,
     name='triagesched',
-    version='0.1',
+    version='0.1.0',
     description='Triage schedule',
     author='Daniel',
     author_email='daniel@gtmanfred.com',
