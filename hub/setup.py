3a4af2942cf8a797ebba68f5a592701cbb67d32f
Daniel Wallace <danielwallace@gtmanfred.com>
2018-04-24 21:54:01 -0500

    grab all mods

--- hub/setup.py
+++ hub/setup.py
@@ -2,6 +2,15 @@
 
 from distutils.core import setup
 
+import os
+
+packages = ['hub']
+
+with os.scandir('hub/mods') as rit:
+    for entry in rit:
+        if entry.name[0] not in ('.', '_') and entry.is_dir():
+            packages.append(f'{entry.path.replace("/", ".")}')
+
 setup(
     name='plugin-hub',
     version='0.1.0',
@@ -9,6 +18,5 @@ setup(
     author='Daniel',
     author_email='daniel@gtmanfred.com',
     url='https://github.com/gtmanfred/plugins',
-    packages=['hub', 'hub.mods.tools'],
+    packages=packages,
 )
-
