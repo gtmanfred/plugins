f038145b99a2ec18997a4c73747d91c61889c4d0
Daniel Wallace <danielwallace@gtmanfred.com>
2018-04-24 21:51:08 -0500

    add bootstrap script for api

---
+++ bootstrap.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+
+for idx, user in enumerate(['daniel', 'megan', 'gareth']):
+    print(requests.post('http://localhost:5000/api/v1/users',
+                 data=json.dumps({'name': user, 'order': idx}),
+                 headers={'Content-Type': 'application/json'}).json())
